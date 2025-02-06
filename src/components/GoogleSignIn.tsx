ac1544b5031a853ecb6b5785ea7985d7a787aede
JonashGana <jonashtiglao@gmail.com>
2025-02-06 23:50:06 +0800

    updated

--- src/components/GoogleSignIn.tsx
+++ src/components/GoogleSignIn.tsx
@@ -5,7 +5,12 @@ const GoogleSignIn = () => {
   return (
     <form action={googleSignIn}>
       <button className="py-2 px-4 w-full border border-zinc-300 rounded-md hover:bg-gray-200 flex gap-4 items-center justify-center">
-        <Image src={"/google.svg"} alt="google icon" height={20} width={20} />
+        <Image
+          src="/assets/google.svg"
+          alt="google icon"
+          height={20}
+          width={20}
+        />
         <span>Google</span>
       </button>
     </form>
