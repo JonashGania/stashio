b07b6db6d92b1a218389dde03a0860efe6c00dad
JonashGana <jonashtiglao@gmail.com>
2025-02-06 23:50:56 +0800

    updated

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,10 +1,12 @@
 import { signOutUser } from "@/actions/auth";
+import { LogOut } from "lucide-react";
 
 const SignOutButton = () => {
   return (
     <form action={signOutUser}>
-      <button className="px-4 py-1 bg-black text-white text-center rounded-2xl">
-        Sign Out
+      <button className="flex items-center gap-2 bg-black w-full rounded-lg px-2 py-1">
+        <LogOut size={18} color="#ffffff" />
+        <span className="text-white text-sm">Log out</span>
       </button>
     </form>
   );
