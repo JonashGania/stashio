3eda9883b00e6afe4bbe91fcd00578e68541b9b9
JonashGana <jonashtiglao@gmail.com>
2025-02-07 23:44:17 +0800

    added getCategories

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,43 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getCategories = () => {
+  return [
+    {
+      title: "Documents",
+      totalFiles: 0,
+      size: 0,
+      url: "/documents",
+      bgColor: "#8F60FF",
+      logo: "/assets/colored-document.svg",
+    },
+    {
+      title: "Images",
+      totalFiles: 0,
+      size: 0,
+      url: "/images",
+      bgColor: "#019BA0",
+      logo: "/assets/colored-images.svg",
+    },
+    {
+      title: "Media",
+      totalFiles: 0,
+      size: 0,
+      url: "/media",
+      bgColor: "#FF7BAC",
+      logo: "/assets/colored-media.svg",
+    },
+    {
+      title: "Others",
+      totalFiles: 0,
+      size: 0,
+      url: "/others",
+      bgColor: "#4E65FB",
+      logo: "/assets/colored-others.svg",
+    },
+  ];
+};
