01a9492aeda10684b7260943ae21a819c26b3448
JonashGana <jonashtiglao@gmail.com>
2025-05-06 22:37:47 +0800

    added auth check

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,14 +1,23 @@
 import { ReactNode } from "react";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 import Sidebars from "@/components/Sidebar";
 import Header from "@/components/Header";
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
   return (
     <main className={`flex min-h-screen w-full dark:bg-gray-950`}>
       <Sidebars />
 
       <section className="flex flex-col h-full w-full flex-1 md:pr-4 lg:pl-4">
-        <Header />
+        <Header user={user} />
         <div className="h-full flex-1">{children}</div>
       </section>
     </main>
