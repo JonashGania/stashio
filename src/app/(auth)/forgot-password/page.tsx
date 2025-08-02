344246ef69ea365df25b6aea4b4830ebf4bef970
JonashGana <jonashtiglao@gmail.com>
2025-08-02 22:59:06 +0800

    forgot password page

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import ResetPasswordForm from "@/components/form/ResetPasswordForm";
+
+const ForgotPassword = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center flex-col px-4">
+      <div className="w-[430px]">
+        <div className="flex justify-center text-center items-center flex-col w-full space-y-2 mb-4">
+          <Image src="/assets/logo.svg" alt="logo" height={50} width={50} />
+          <h1 className="text-2xl font-semibold text-gray-800">
+            Reset your password
+          </h1>
+          <p className="text-gray-600">
+            Enter your email address and we&apos;ll send you a link to reset
+            your password
+          </p>
+        </div>
+        <Card className="w-full pt-8 pb-6 shadow-md border-gray-100 rounded-2xl">
+          <CardContent className="p-0 px-6">
+            <ResetPasswordForm />
+            <div className="flex items-center justify-center mt-6 text-sm">
+              <Link
+                href="/sign-in"
+                className="flex items-center gap-2 text-gray-600 hover:text-neutral-900"
+              >
+                <ArrowLeft size={18} />
+                Back to login
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
