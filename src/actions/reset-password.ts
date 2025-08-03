d4194c40f0f86c162c7a84cbaed048859c359c3a
JonashGana <jonashtiglao@gmail.com>
2025-08-03 18:08:04 +0800

    update resetPassword used nodemailer

--- src/actions/reset-password.ts
+++ src/actions/reset-password.ts
@@ -1,20 +1,20 @@
 "use server";
 
-import { Resend } from "resend";
 import ResetPasswordTemplate from "@/components/email/reset-password-template";
 import { generatePasswordResetToken } from "@/lib/utils";
 import { z } from "zod";
 import { ResetSchema } from "@/schemas";
 import { getUserFromDb } from "./auth";
+import nodemailer from "nodemailer";
+import { render } from "@react-email/render";
 
 const domain =
   process.env.NODE_ENV === "production"
     ? process.env.NEXT_PUBLIC_PROD_URL
     : process.env.NEXT_PUBLIC_DEV_URL;
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export const resetPassword = async (data: z.infer<typeof ResetSchema>) => {
+  const emailSender = process.env.SMTP_USERNAME;
   try {
     const { email } = await ResetSchema.parseAsync(data);
 
@@ -25,17 +25,41 @@ export const resetPassword = async (data: z.infer<typeof ResetSchema>) => {
 
     const passwordResetToken = await generatePasswordResetToken(email);
 
-    const response = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: email,
-      subject: "Hello world",
-      react: ResetPasswordTemplate({
-        userFirstname: existingUser.name,
-        resetPasswordLink: `${domain}/reset-password?token=${passwordResetToken.token}`,
-      }),
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: emailSender,
+        pass: process.env.SMTP_PASSWORD,
+      },
     });
 
-    console.log(response);
+    const resetLink = `${domain}/reset-password?token=${passwordResetToken.token}`;
+
+    const emailHtml = await render(
+      ResetPasswordTemplate({
+        userFirstname: existingUser.name,
+        resetPasswordLink: resetLink,
+      })
+    );
+
+    const mailOptions = {
+      from: emailSender,
+      to: email,
+      subject: "Password Reset Link",
+      html: emailHtml,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    // const response = await resend.emails.send({
+    //   from: "Acme <onboarding@resend.dev>",
+    //   to: email,
+    //   subject: "Hello world",
+    //   react: ResetPasswordTemplate({
+    //     userFirstname: existingUser.name,
+    //     resetPasswordLink: `${domain}/reset-password?token=${passwordResetToken.token}`,
+    //   }),
+    // });
 
     return {
       success: true,
