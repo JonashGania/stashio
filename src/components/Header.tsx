c38ff6a1677f8678b5ce089a01b7aded7971da3c
JonashGana <jonashtiglao@gmail.com>
2025-08-20 23:20:13 +0800

    changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,38 +20,38 @@ const Header = async ({ user }: { user: User }) => {
     <header className="w-full flex gap-4 justify-between py-3 px-6">
       <Searchbar userId={user.id} />
 
-      <div className="hidden md:flex gap-4 items-center ">
+      <div className="flex gap-4 items-center">
         <UploadButton userId={user.id} />
         <ThemeButton />
-
-        <DropdownMenu>
-          <DropdownMenuTrigger className="flex items-center flex-shrink-0 gap-1 rounded-xl hover:bg-zinc-200 dark:hover:bg-zinc-800 pr-1 outline-none">
-            <Avatar user={user} />
-            <ChevronDown size={15} />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="px-4 py-2 overflow-hidden w-[200px]"
-            align="end"
-          >
-            <div className="flex gap-2 items-center mb-3">
+        <div className="hidden md:block ">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center flex-shrink-0 gap-1 rounded-xl hover:bg-zinc-200 dark:hover:bg-zinc-800 pr-1 outline-none">
               <Avatar user={user} />
-              <div className="flex flex-col gap-0 max-w-[130px]">
-                <span className="font-medium text-sm capitalize">
-                  {user?.name}
-                </span>
-                <span className="text-xs text-zinc-600 dark:text-zinc-300 overflow-hidden text-ellipsis">
-                  {user?.email}
-                </span>
+              <ChevronDown size={15} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="px-4 py-2 overflow-hidden w-[200px]"
+              align="end"
+            >
+              <div className="flex gap-2 items-center mb-3">
+                <Avatar user={user} />
+                <div className="flex flex-col gap-0 max-w-[130px]">
+                  <span className="font-medium text-sm capitalize">
+                    {user?.name}
+                  </span>
+                  <span className="text-xs text-zinc-600 dark:text-zinc-300 overflow-hidden text-ellipsis">
+                    {user?.email}
+                  </span>
+                </div>
               </div>
-            </div>
-            <SignOutButton position="header" />
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+              <SignOutButton position="header" />
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
 
-      <div className="flex md:hidden items-center gap-2">
-        <ThemeButton />
-        <MobileNavigation user={user} storageInfo={storageInfo} />
+        <div className="block md:hidden">
+          <MobileNavigation user={user} storageInfo={storageInfo} />
+        </div>
       </div>
     </header>
   );
