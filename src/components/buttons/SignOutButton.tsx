8204e10c430cc9520cfb295795d0bc5241b589ad
JonashGana <jonashtiglao@gmail.com>
2025-08-20 23:22:27 +0800

    change background color

--- src/components/buttons/SignOutButton.tsx
+++ src/components/buttons/SignOutButton.tsx
@@ -5,15 +5,15 @@ const SignOutButton = ({ position }: { position: string }) => {
   return (
     <form action={signOutUser}>
       <button
-        className={`flex items-center gap-2 bg-black dark:bg-white w-full  px-3 ${
+        className={`flex items-center gap-2 bg-gradient-to-br w-full from-purple-500 to-pink-500  px-3 ${
           position === "mobile-nav"
             ? `rounded-3xl py-2 justify-center`
             : `rounded-md py-1`
         }`}
       >
-        <LogOut size={18} className="text-white dark:text-black" />
+        <LogOut size={18} className="text-white " />
         <span
-          className={`text-white dark:text-black ${
+          className={`text-white  ${
             position === "mobile-nav" ? "text-base" : "text-sm"
           }`}
         >
