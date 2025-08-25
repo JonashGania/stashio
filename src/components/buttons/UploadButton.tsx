2ad8919a1ef42d6c1559b3a696e9d5e201996261
JonashGana <jonashtiglao@gmail.com>
2025-08-25 19:55:14 +0800

    minor ui change

--- src/components/buttons/UploadButton.tsx
+++ src/components/buttons/UploadButton.tsx
@@ -103,9 +103,9 @@ const UploadButton = ({ userId }: { userId: string | undefined }) => {
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
-      <button className="flex items-center gap-3  bg-white px-6 py-2 rounded-3xl shadow-sm shadow-zinc-500 hover:bg-[rgba(221,214,254,0.4)] dark:hover:bg-white hover:shadow-[0px_2px_4px_1px_rgba(145,145,145,0.75)] dark:hover:shadow-sm">
-        <Upload color="#52525b" size={20} strokeWidth={1.7} />
-        <span className="font-normal text-zinc-600">Upload</span>
+      <button className="flex items-center gap-2  bg-white px-4 py-2 bg-gradient-to-br from-purple-500 to-pink-500 rounded-lg">
+        <Upload color="#ffffff" size={20} strokeWidth={2} />
+        <span className=" text-white text-sm ">Upload</span>
       </button>
 
       {files.length > 0 && (
