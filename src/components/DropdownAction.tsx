75f0cad7869b211f5899745ea266201c698c8981
JonashGana <jonashtiglao@gmail.com>
2025-08-25 19:54:43 +0800

    minor ui change

--- src/components/DropdownAction.tsx
+++ src/components/DropdownAction.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import clsx from "clsx";
 import { Files } from "@/types";
 import {
   DropdownMenu,
@@ -18,52 +19,68 @@ interface DropdownActionProps {
   layout: string;
   files: Files;
   userId: string;
+  className?: string;
 }
 
-const DropdownAction = ({ layout, files, userId }: DropdownActionProps) => {
+const DropdownAction = ({
+  layout,
+  files,
+  userId,
+  className,
+}: DropdownActionProps) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className=" dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-200 w-7 h-7 rounded-md flex justify-center items-center">
+        <button
+          className={clsx(
+            " transition-colors duration-200 w-7 h-7 rounded-md flex justify-center items-center",
+            className
+          )}
+        >
           {layout === "table" ? (
-            <Ellipsis size={15} className="text-gray-700 dark:text-zinc-300 " />
+            <Ellipsis
+              size={15}
+              className={`text-gray-700 ${className ? "dark:text-gray-700" : "dark:text-zinc-300"}`}
+            />
           ) : (
             <EllipsisVertical
               size={20}
-              className="text-zinc-600 dark:text-zinc-300"
+              className={`text-zinc-700 ${className ? "dark:text-gray-700" : "dark:text-zinc-300"}`}
             />
           )}
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent
         align="end"
-        className="w-40 shadow-zinc-400 shadow dark:shadow-none"
+        className="w-48 shadow-zinc-400 shadow dark:shadow-none rounded-2xl py-2 px-0"
       >
         <DropdownMenuItem
-          className="focus:bg-zinc-300 dark:focus:bg-zinc-800"
+          className="focus:bg-indigo-200/60 dark:focus:bg-zinc-800"
           onSelect={(e) => e.preventDefault()}
         >
           <DetailsDialog file={files} />
         </DropdownMenuItem>
         <DropdownMenuItem
-          className="focus:bg-zinc-300 dark:focus:bg-zinc-800"
+          className="focus:bg-indigo-200/60 dark:focus:bg-zinc-800"
           onSelect={(e) => e.preventDefault()}
         >
           <RenameDialog file={files} userId={userId} />
         </DropdownMenuItem>
-        <DropdownMenuItem className="focus:bg-zinc-300 dark:focus:bg-zinc-800">
+        <DropdownMenuItem className="focus:bg-indigo-200/60 dark:focus:bg-zinc-800">
           <Link
             href={getDownloadUrl(files.fileId)}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-[6px]"
+            className="flex items-center gap-4 px-2 w-full"
           >
-            <Download size={16} />
-            <span className="text-zinc-600 dark:text-gray-200">Download</span>
+            <Download size={20} className="text-black dark:text-gray-200" />
+            <span className="text-black font-semibold dark:text-gray-200">
+              Download
+            </span>
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem
-          className="focus:bg-zinc-300 dark:focus:bg-zinc-800"
+          className="focus:bg-indigo-200/60 dark:focus:bg-zinc-800"
           onSelect={(e) => e.preventDefault()}
         >
           <DeleteDialog file={files} userId={userId} />
